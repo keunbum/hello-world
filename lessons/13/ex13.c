161af3cc06ec36210a09942c260f781186f65521
keunbum <rkeunbum@gmail.com>
2022-02-12 20:23:55 +0900

    Added smth to lessson/13

--- lessons/13/ex13.c
+++ lessons/13/ex13.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+#define min(a,b) a < b ? a : b
+
 int main(int argc, char *argv[]) {
 	int i = 0;
 	for (i = 1; i < argc; i++) {
 		printf("arg %d: %s\n", i, argv[i]);
 	}
-	const char *states[] = {
-		"California", "Oregon", "Washington", "Texas"
+	char *states[] = {
+		"California", "Oregon", "Washington", "Texas", NULL
 	};
 	int num_states = sizeof(states) / sizeof(states[0]);
+	int mn = min(argc, num_states);
+	for (i = 0; i < mn; i++) {
+		states[i] = argv[i];
+	}
+	/*for (i = 1; i < argc; i++) {
+		printf("arg %d: %s\n", i, argv[i]);
+	}*/
 	for (i = 0; i < num_states; i++) {
 		printf("state %d: %s\n", i, states[i]);
 	}
